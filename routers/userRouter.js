65f35391abde9aa124a141a8912f617d3af3ef8a
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-13 17:53:46 -0500

    completes mvp

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const db = require('../data/helpers')
+const restricted = require('../middleware/restricted-middleware')
 
 
-router.get('/', (req, res) => {
+router.get('/', restricted, (req, res) => {
     db.find()
     .then(users => res.status(200).json(users))
     .catch(err => res.status(500).json({ error: 'Could not get users' }))
