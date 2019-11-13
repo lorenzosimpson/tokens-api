13bb2d55b703087e605b939af7245db0ff162a1f
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-13 15:50:09 -0500

    creates table and seed

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,21 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './data/'
-    }
+      filename: './data/user-db.db3'
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
   },
 
   staging: {
